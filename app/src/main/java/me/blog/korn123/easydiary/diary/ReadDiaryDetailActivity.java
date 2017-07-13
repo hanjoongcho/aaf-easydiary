6e3d18dcbc500f32e7f499766e98d91d14564e9e
hanjoong.cho <hanjoongcho@gmail.com>
2017-07-13 11:14:46 +0900

    [기능추가] 다이어리 상세보기 viewpager 개발중

--- app/src/main/java/me/blog/korn123/easydiary/diary/ReadDiaryDetailActivity.java
+++ app/src/main/java/me/blog/korn123/easydiary/diary/ReadDiaryDetailActivity.java
@@ -1,8 +1,6 @@
 package me.blog.korn123.easydiary.diary;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,16 +10,13 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -44,7 +39,6 @@ import java.util.Locale;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.realm.RealmList;
 import me.blog.korn123.commons.constants.Constants;
 import me.blog.korn123.commons.utils.BitmapUtils;
 import me.blog.korn123.commons.utils.CommonUtils;
@@ -79,7 +73,11 @@ public class ReadDiaryDetailActivity extends EasyDiaryActivity {
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        // fixme elegance
+        // activity destroy 시 저장된 savedInstance 값이 전달되면 갱신된 fragment 접근이 안됨
+//        super.onCreate(savedInstanceState);
+        super.onCreate(null);
+
         setContentView(R.layout.activity_read_diary_detail);
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
@@ -191,6 +189,8 @@ public class ReadDiaryDetailActivity extends EasyDiaryActivity {
     protected void onPause() {
         super.onPause();
         destroyModule();
+        PlaceholderFragment fragment = mSectionsPagerAdapter.getFragment(mViewPager.getCurrentItem());
+        getIntent().putExtra("sequence", fragment.mSequence);
     }
 
     @Override
