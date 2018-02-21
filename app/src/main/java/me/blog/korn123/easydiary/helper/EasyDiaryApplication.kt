0162ab2544a3cf97de61638b98ae6abd5e77dfa4
bulbarsaur <hanjoongcho@gmail.com>
2018-02-21 09:20:33 +0900

    Added LeakCanary, a memory leak detection library for Android.

--- app/src/main/java/me/blog/korn123/easydiary/helper/EasyDiaryApplication.kt
+++ app/src/main/java/me/blog/korn123/easydiary/helper/EasyDiaryApplication.kt
@@ -1,8 +1,10 @@
 package me.blog.korn123.easydiary.helper
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 
 import io.realm.Realm
+import me.blog.korn123.easydiary.BuildConfig
 
 /**
  * Created by CHO HANJOONG on 2017-03-16.
@@ -11,6 +13,12 @@ import io.realm.Realm
 class EasyDiaryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                return
+            }
+            LeakCanary.install(this)
+        }
         Realm.init(this)
     }
 }
