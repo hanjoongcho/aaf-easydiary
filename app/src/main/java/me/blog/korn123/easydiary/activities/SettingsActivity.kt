060f11757b6c8d384d0c0dcebd6e6f47647068f7
hanjoong.cho <hanjoongcho@gmail.com>
2018-03-02 21:44:18 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easydiary/activities/SettingsActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/SettingsActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
 import android.support.v7.app.AlertDialog
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -51,7 +50,7 @@ class SettingsActivity : EasyDiaryActivity() {
 
         setSupportActionBar(toolbar)
         supportActionBar?.run {
-            setTitle(R.string.setting_title)
+            setTitle(R.string.settings)
             setDisplayHomeAsUpEnabled(true)
         }
     }
