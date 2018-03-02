6ad3b5c313ab1fc69f68e5dbafede58ce7c20604
hanjoong.cho <hanjoongcho@gmail.com>
2018-03-02 21:40:01 +0900

    Changing diary photo attachment method

--- app/src/main/java/me/blog/korn123/easydiary/models/PhotoUriDto.kt
+++ app/src/main/java/me/blog/korn123/easydiary/models/PhotoUriDto.kt
@@ -1,7 +1,10 @@
 package me.blog.korn123.easydiary.models
 
+import android.os.Environment
 import io.realm.RealmObject
+import me.blog.korn123.commons.constants.Path
 import me.blog.korn123.easydiary.helper.CONTENT_URI_PREFIX
+import org.apache.commons.io.FilenameUtils
 import org.apache.commons.lang3.StringUtils
 
 /**
@@ -19,5 +22,7 @@ open class PhotoUriDto : RealmObject {
     
     fun isContentUri(): Boolean = StringUtils.startsWith(photoUri, CONTENT_URI_PREFIX)
     
-    fun getFilePath(): String = StringUtils.substring(photoUri, 6)
+    fun getFilePath(): String {
+        return "${Environment.getExternalStorageDirectory().absolutePath}${Path.DIARY_PHOTO_DIRECTORY}${FilenameUtils.getBaseName(photoUri)}"
+    } 
 }
