9d30745f64c2c89c3ebafc53226daa3f6b692968
hanjoong.cho <hanjoongcho@gmail.com>
2018-03-21 22:24:20 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easydiary/adapters/DiaryMainItemAdapter.kt
+++ app/src/main/java/me/blog/korn123/easydiary/adapters/DiaryMainItemAdapter.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.graphics.drawable.GradientDrawable
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.ColorUtils
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
