4f07ba59e85ff072f2f2e1ff97c99f89f5552fd2
hanjoong.cho <hanjoongcho@gmail.com>
2018-03-25 21:12:12 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easydiary/activities/SettingsActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/SettingsActivity.kt
@@ -123,13 +123,27 @@ class SettingsActivity : EasyDiaryActivity() {
         super.onResume()
         initPreference()
         setFontsStyle()
-
+        setupInvite()
+        
         if (BaseConfig(this).isThemeChanged) {
             BaseConfig(this).isThemeChanged = false
             val readDiaryIntent = Intent(this, DiaryMainActivity::class.java)
             readDiaryIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(readDiaryIntent)
             this.overridePendingTransition(0, 0)
+        }
+    }
+
+    private fun setupInvite() {
+        invite.setOnClickListener {
+            val text = String.format(getString(io.github.hanjoongcho.commons.R.string.share_text), getString(R.string.app_name), getStoreUrl())
+            Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name))
+                putExtra(Intent.EXTRA_TEXT, text)
+                type = "text/plain"
+                startActivity(Intent.createChooser(this, getString(io.github.hanjoongcho.commons.R.string.invite_via)))
+            }
         }
     }
 
@@ -248,6 +262,8 @@ class SettingsActivity : EasyDiaryActivity() {
         lockNumberSettingSummary.text = "${getString(R.string.lock_number)} ${config.aafPinLockSavedPassword}"
         rateAppSettingSummary.text = String.format("Easy Diary v %s", BuildConfig.VERSION_NAME)
     }
+
+    private fun getStoreUrl() = "https://play.google.com/store/apps/details?id=$packageName"
 
     companion object {
         private var mTaskFlag = 0
