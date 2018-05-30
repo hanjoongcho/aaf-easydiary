4d1c64ef2d16dd7a889ed39a4a458b8fc0a7c67a
hanjoong.cho <hanjoongcho@gmail.com>
2018-05-31 07:12:13 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easydiary/viewholders/PostcardViewHolder.kt
+++ app/src/main/java/me/blog/korn123/easydiary/viewholders/PostcardViewHolder.kt
@@ -9,9 +9,6 @@ import io.github.aafactory.commons.utils.CommonUtils
 import me.blog.korn123.easydiary.R
 import java.io.File
 
-/**
- * ViewHolder that represents a cat image.
- */
 internal class PostcardViewHolder(itemView: View, val activity: Activity) : RecyclerView.ViewHolder(itemView) {
 
     private val imageView: ImageView = itemView.findViewById(R.id.imageview)
@@ -19,17 +16,10 @@ internal class PostcardViewHolder(itemView: View, val activity: Activity) : Recy
     internal fun bindTo(file: File) {
         val point =  CommonUtils.getDefaultDisplay(activity)
         val targetX = Math.floor((point.x - CommonUtils.dpToPixelFloatValue(imageView.context, 9)) / 2.0)
-//        imageView.layoutParams.width = targetX.toInt()
         imageView.layoutParams.height = targetX.toInt()
-//        //        imageView.setImageBitmap(BitmapUtils.decodeFileMaxWidthHeight(file.path, 500))
         Glide.with(imageView.context)
                 .load(file)
 //                .apply(RequestOptions().placeholder(R.drawable.ic_aaf_photos).fitCenter())
                 .into(imageView)
-
-//        val lp = imageView.layoutParams
-//        if (lp is FlexboxLayoutManager.LayoutParams) {
-//            lp.flexGrow = 1f
-//        }
     }
 }
