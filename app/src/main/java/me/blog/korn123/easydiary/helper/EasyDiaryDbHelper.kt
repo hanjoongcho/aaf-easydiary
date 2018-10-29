57db9074f5d00a0864271750a7fb512211874645
hanjoong.cho <hanjoongcho@gmail.com>
2018-10-30 01:22:21 +0900

    Improved diary creation time input function

--- app/src/main/java/me/blog/korn123/easydiary/helper/EasyDiaryDbHelper.kt
+++ app/src/main/java/me/blog/korn123/easydiary/helper/EasyDiaryDbHelper.kt
@@ -14,7 +14,7 @@ object EasyDiaryDbHelper {
     private val mDiaryConfig: RealmConfiguration by lazy {
         RealmConfiguration.Builder()
                 .name("diary.realm")
-                .schemaVersion(6)
+                .schemaVersion(7)
                 .migration(EasyDiaryMigration())
                 .modules(Realm.getDefaultModule())
                 .build()
