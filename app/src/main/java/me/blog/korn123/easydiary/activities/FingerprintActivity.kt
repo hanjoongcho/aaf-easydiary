4b182da52beb4b610501423f0d7ae9bc3f2798b4
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2018-12-09 01:00:04 +0900

    Implementing lock function using fingerprint recognition

--- app/src/main/java/me/blog/korn123/easydiary/activities/FingerprintActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/FingerprintActivity.kt
@@ -1,28 +1,36 @@
 package me.blog.korn123.easydiary.activities
 
+import android.annotation.TargetApi
 import android.app.KeyguardManager
 import android.hardware.fingerprint.FingerprintManager
 import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyPermanentlyInvalidatedException
 import android.security.keystore.KeyProperties
+import android.support.annotation.RequiresApi
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import io.github.aafactory.commons.activities.BaseSimpleActivity
-import kotlinx.android.synthetic.main.activity_lock_setting.*
+import kotlinx.android.synthetic.main.activity_fingerprint.*
 import me.blog.korn123.commons.utils.FontUtils
 import me.blog.korn123.easydiary.R
-import java.security.KeyStore
-import java.security.KeyStoreException
-import java.security.NoSuchAlgorithmException
-import java.security.NoSuchProviderException
+import java.io.IOException
+import java.security.*
+import java.security.cert.CertificateException
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.NoSuchPaddingException
+import javax.crypto.SecretKey
 
 class FingerprintActivity : BaseSimpleActivity() {
-    private var mKeyStore: KeyStore? = null
-    private var mKeyGenerator: KeyGenerator? = null
+    private lateinit var mKeyStore: KeyStore
+    private lateinit var mKeyGenerator: KeyGenerator
+    private lateinit var mFingerprintManager: FingerprintManager
+    private lateinit var mCryptoObject: FingerprintManager.CryptoObject
+    private lateinit var mCancellationSignal: CancellationSignal
     
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,7 +68,7 @@ class FingerprintActivity : BaseSimpleActivity() {
 
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
             val keyguardManager = getSystemService(KeyguardManager::class.java)
-            val fingerprintManager = getSystemService(FingerprintManager::class.java)
+            mFingerprintManager = getSystemService(FingerprintManager::class.java)
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
 //                purchaseButtonNotInvalidated.setEnabled(true)
@@ -88,7 +96,7 @@ class FingerprintActivity : BaseSimpleActivity() {
             // See http://developer.android.com/reference/android/Manifest.permission.html#USE_FINGERPRINT
             // The line below prevents the false positive inspection from Android Studio
             // noinspection ResourceType
-            if (!fingerprintManager.hasEnrolledFingerprints()) {
+            if (!mFingerprintManager.hasEnrolledFingerprints()) {
 //                purchaseButton.setEnabled(false)
                 // This happens when no fingerprints are registered.
                 Toast.makeText(this,
@@ -96,11 +104,20 @@ class FingerprintActivity : BaseSimpleActivity() {
                         Toast.LENGTH_LONG).show()
                 return
             }
-//            createKey(DEFAULT_KEY_NAME, true)
+            createKey(DEFAULT_KEY_NAME, true)
 //            createKey(KEY_NAME_NOT_INVALIDATED, false)
 //            purchaseButton.setEnabled(true)
 //            purchaseButton.setOnClickListener(
 //                    PurchaseButtonClickListener(defaultCipher, DEFAULT_KEY_NAME))
+
+            // fingerprint 인증시작
+            // Set up the crypto object for later. The object will be authenticated by use
+            // of the fingerprint.
+            if (initCipher(defaultCipher, DEFAULT_KEY_NAME)) {
+                mCryptoObject = FingerprintManager.CryptoObject(defaultCipher)
+            } else {
+
+            }
         }
     }
 
@@ -108,7 +125,147 @@ class FingerprintActivity : BaseSimpleActivity() {
         isBackgroundColorFromPrimaryColor = true
         super.onResume()
         FontUtils.setFontsTypeface(applicationContext, assets, null, container)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            startListening(mCryptoObject)
+        }
     }
 
+    override fun onPause() {
+        super.onPause()
+        mCancellationSignal.cancel()
+    }
+    
     override fun getMainViewGroup(): ViewGroup? = container
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun startListening(cryptoObject: FingerprintManager.CryptoObject) {
+        if (!isFingerprintAuthAvailable()) {
+            return
+        }
+        mCancellationSignal = CancellationSignal()
+//        mSelfCancelled = false
+        // The line below prevents the false positive inspection from Android Studio
+
+        mFingerprintManager
+                .authenticate(cryptoObject, mCancellationSignal, 0 /* flags */, object : FingerprintManager.AuthenticationCallback() {
+                    override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
+                        super.onAuthenticationSucceeded(result)
+                        guideMessage.text = "onAuthenticationSucceeded"
+                    }
+
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                        super.onAuthenticationError(errorCode, errString)
+                        guideMessage.text = "onAuthenticationError"
+                    }
+
+                    override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
+                        super.onAuthenticationHelp(helpCode, helpString)
+                        guideMessage.text = "onAuthenticationHelp"
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        guideMessage.text = "onAuthenticationFailed"
+                    }
+                }, null)
+    }
+    
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isFingerprintAuthAvailable(): Boolean {
+        // The line below prevents the false positive inspection from Android Studio
+
+        return mFingerprintManager.isHardwareDetected() && mFingerprintManager.hasEnrolledFingerprints()
+    }
+    
+    /**
+     * Creates a symmetric key in the Android Key Store which can only be used after the user has
+     * authenticated with fingerprint.
+     *
+     * @param keyName the name of the key to be created
+     * @param invalidatedByBiometricEnrollment if `false` is passed, the created key will not
+     * be invalidated even if a new fingerprint is enrolled.
+     * The default value is `true`, so passing
+     * `true` doesn't change the behavior
+     * (the key will be invalidated if a new fingerprint is
+     * enrolled.). Note that this parameter is only valid if
+     * the app works on Android N developer preview.
+     */
+//    @TargetApi(Build.VERSION_CODES.M)
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun createKey(keyName: String, invalidatedByBiometricEnrollment: Boolean) {
+        // The enrolling flow for fingerprint. This is where you ask the user to set up fingerprint
+        // for your flow. Use of keys is necessary if you need to know if the set of
+        // enrolled fingerprints has changed.
+        try {
+            mKeyStore.load(null)
+            // Set the alias of the entry in Android KeyStore where the key will appear
+            // and the constrains (purposes) in the constructor of the Builder
+
+            val builder = KeyGenParameterSpec.Builder(keyName,
+                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    // Require the user to authenticate with a fingerprint to authorize every use
+                    // of the key
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+
+            // This is a workaround to avoid crashes on devices whose API level is < 24
+            // because KeyGenParameterSpec.Builder#setInvalidatedByBiometricEnrollment is only
+            // visible on API level +24.
+            // Ideally there should be a compat library for KeyGenParameterSpec.Builder but
+            // which isn't available yet.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                builder.setInvalidatedByBiometricEnrollment(invalidatedByBiometricEnrollment)
+            }
+            mKeyGenerator.init(builder.build())
+            mKeyGenerator.generateKey()
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        } catch (e: InvalidAlgorithmParameterException) {
+            throw RuntimeException(e)
+        } catch (e: CertificateException) {
+            throw RuntimeException(e)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    /**
+     * Initialize the [Cipher] instance with the created key in the
+     * [.createKey] method.
+     *
+     * @param keyName the key name to init the cipher
+     * @return `true` if initialization is successful, `false` if the lock screen has
+     * been disabled or reset after the key was generated, or if a fingerprint got enrolled after
+     * the key was generated.
+     */
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun initCipher(cipher: Cipher, keyName: String): Boolean {
+        try {
+            mKeyStore.load(null)
+            val key = mKeyStore.getKey(keyName, null) as SecretKey
+            cipher.init(Cipher.ENCRYPT_MODE, key)
+            return true
+        } catch (e: KeyPermanentlyInvalidatedException) {
+            return false
+        } catch (e: KeyStoreException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: CertificateException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: UnrecoverableKeyException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: IOException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: InvalidKeyException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        }
+
+    }
+    
+    companion object {
+        const val DEFAULT_KEY_NAME = "default_key"
+    }
 }
