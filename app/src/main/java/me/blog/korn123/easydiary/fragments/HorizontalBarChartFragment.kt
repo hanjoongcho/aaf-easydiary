6f1d644fcf8148e4bcc8cc3fdac66b94c6a8df0d
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2019-06-11 07:34:17 +0900

    Improve the function of diary chart

--- app/src/main/java/me/blog/korn123/easydiary/fragments/HorizontalBarChartFragment.kt
+++ app/src/main/java/me/blog/korn123/easydiary/fragments/HorizontalBarChartFragment.kt
@@ -89,7 +89,7 @@ class HorizontalBarChartFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_barchart, container, false)
+        return inflater.inflate(R.layout.fragment_horizontal_barchart, container, false)
     }
 
     private fun setData(count: Int, range: Float) {
