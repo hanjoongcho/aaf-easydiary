75ebb1da38c663c9d394623aaa5f77ac5cb9d28c
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2019-12-05 01:15:49 +0900

    Dashboard function is being implemented

--- app/src/main/java/me/blog/korn123/easydiary/activities/DashboardActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/DashboardActivity.kt
@@ -8,6 +8,7 @@ import me.blog.korn123.commons.utils.ChartUtils
 import me.blog.korn123.commons.utils.FlavorUtils
 import me.blog.korn123.easydiary.R
 import me.blog.korn123.easydiary.helper.EasyDiaryDbHelper
+import java.util.*
 
 /**
  * Created by CHO HANJOONG on 2017-03-16.
@@ -38,6 +39,13 @@ class DashboardActivity : EasyDiaryActivity() {
 
         val symbolMap = FlavorUtils.getDiarySymbolMap(this)
         val sortedMap = ChartUtils.getSortedMapBySymbol(true)
+
+        val calendar = Calendar.getInstance()
+        val endMillis = calendar.timeInMillis
+        calendar.add(Calendar.DATE, -7)
+        val startMillis = calendar.timeInMillis
+
+        val sortedMapLastWeek = ChartUtils.getSortedMapBySymbol(true, startMillis, endMillis)
         sortedMap.entries.forEachIndexed { index, entry ->
             when (index) {
                 0 -> {
@@ -62,7 +70,32 @@ class DashboardActivity : EasyDiaryActivity() {
                 }
             }
             countOfLifetime.text = EasyDiaryDbHelper.readDiary(null).size.toString()
-            Log.i("aaf-t", "$index, $entry")
+        }
+
+        sortedMapLastWeek.entries.forEachIndexed { index, entry ->
+            when (index) {
+                0 -> {
+                    FlavorUtils.initWeatherView(this, symbolRank5, entry.key)
+                    descriptionRank5.text = symbolMap[entry.key]
+                    countRank5.text = "${entry.value}"
+                }
+                1 -> {
+                    FlavorUtils.initWeatherView(this, symbolRank6, entry.key)
+                    descriptionRank6.text = symbolMap[entry.key]
+                    countRank6.text = "${entry.value}"
+                }
+                2 -> {
+                    FlavorUtils.initWeatherView(this, symbolRank7, entry.key)
+                    descriptionRank7.text = symbolMap[entry.key]
+                    countRank7.text = "${entry.value}"
+                }
+                3 -> {
+                    FlavorUtils.initWeatherView(this, symbolRank8, entry.key)
+                    descriptionRank8.text = symbolMap[entry.key]
+                    countRank8.text = "${entry.value}"
+                }
+            }
+            countOfLastWeek.text = EasyDiaryDbHelper.readDiary(null, false, startMillis, endMillis).size.toString()
         }
     }
 
