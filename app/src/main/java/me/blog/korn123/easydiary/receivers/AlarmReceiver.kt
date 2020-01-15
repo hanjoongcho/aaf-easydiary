4c1128b8545abf3e72e4a3191b5ff0ff0c78de0b
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-01-15 23:50:43 +0900

    Update debug activity for development

--- app/src/main/java/me/blog/korn123/easydiary/receivers/AlarmReceiver.kt
+++ app/src/main/java/me/blog/korn123/easydiary/receivers/AlarmReceiver.kt
@@ -4,16 +4,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import me.blog.korn123.easydiary.activities.Alarm
-import me.blog.korn123.easydiary.activities.isScreenOn
+import me.blog.korn123.easydiary.activities.DevActivity
 import me.blog.korn123.easydiary.activities.showAlarmNotification
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (context.isScreenOn()) {
-            val alarm = Alarm(1000, 0, 0, isEnabled = false, vibrate = false, soundTitle = "", soundUri = "", label = "")
-            context.showAlarmNotification(alarm)
-        } else {
-
-        }
+        val alarm = Alarm(intent.getIntExtra(DevActivity.ALARM_ID, -1), 0, 0, isEnabled = false, vibrate = false, soundTitle = "", soundUri = "", label = "")
+        context.showAlarmNotification(alarm)
     }
 }
