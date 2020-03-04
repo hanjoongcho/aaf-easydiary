6b79211c469a9be154592ff023a40ef08c1e20c2
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-03-04 18:29:27 +0900

    Update PrepareReleaseTest

--- app/src/test/java/me/blog/korn123/commons/utils/PrepareReleaseTest.kt
+++ app/src/test/java/me/blog/korn123/commons/utils/PrepareReleaseTest.kt
@@ -10,18 +10,40 @@ class PrepareReleaseTest {
     @Test
     @Throws(Exception::class)
     fun determine_strings_xml() {
-        var defaultLanguages: List<String>? = null
-        File("./src/main/res/").listFiles().map {
-            if (it.name.startsWith("values")) {
-                if (it.name == "values") {
-                    defaultLanguages = FileUtils.readLines(File(it.absolutePath + "/strings.xml"), StandardCharsets.UTF_8)
-                    defaultLanguages?.let {
-                        println(it.size)
-                    }
+        var valuesDefault: List<String>?
+        var startIndex = -1
+        var endIndex = -1
+        File("./src/main/res/").listFiles()?.map { localeFolder ->
+            if (localeFolder.name.startsWith("values")) {
+                if (localeFolder.name == "values") {
+                    valuesDefault = FileUtils.readLines(File(localeFolder.absolutePath + "/strings.xml"), StandardCharsets.UTF_8)
+                    println("Total lines: ${valuesDefault?.size}")
 
+                    valuesDefault?.forEachIndexed { index, s ->
+                        if (s.contains("release_")) {
+                            val version = s.split("\"")[1].split("_")[1].toIntOrNull() ?: -1
+                            println("$index: ${s.split("\"")[1]} version is $version")
+                            if (version > 0 && index > startIndex) {
+                                startIndex = index
+                            }
+                        }
+                    }
+                    valuesDefault?.let {
+                        for (lineNum in startIndex until it.size) {
+                            if (it[lineNum].contains("</string>")) {
+                                endIndex = lineNum
+                                break
+                            }
+                        }
+                        println("Current release: $startIndex ~ $endIndex")
+
+                        for (lineNum in startIndex until endIndex.plus(1)) {
+                            println(it[lineNum])
+                        }
+                    }
                 }
 
-                it.listFiles().map { targetFile ->
+                localeFolder.listFiles()?.map { targetFile ->
                     if (targetFile.name == "strings.xml") {
                         val lines = FileUtils.readLines(targetFile, StandardCharsets.UTF_8)
                         println(targetFile.absolutePath + ": " + lines.size)
