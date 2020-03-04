511da21fbecba57e217156ad908d4785902c9faf
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-03-04 18:55:38 +0900

    Update PrepareReleaseTest

--- app/src/test/java/me/blog/korn123/commons/utils/PrepareReleaseTest.kt
+++ app/src/test/java/me/blog/korn123/commons/utils/PrepareReleaseTest.kt
@@ -7,6 +7,38 @@ import java.io.File
 import java.nio.charset.StandardCharsets
 
 class PrepareReleaseTest {
+
+    private fun determineLastReleaseStartLine(values: List<String>?): Int {
+        var startIndex = -1
+        values?.forEachIndexed { index, s ->
+            if (s.contains("release_")) {
+                val version = s.split("\"")[1].split("_")[1].toIntOrNull() ?: -1
+//                println("$index: ${s.split("\"")[1]} version is $version")
+                if (version > 0 && index > startIndex) {
+                    startIndex = index
+                }
+            }
+        }
+        return startIndex
+    }
+
+    private fun determineLastReleaseEndLine(values: List<String>?, startIndex: Int): Int {
+        var endIndex = -1
+        values?.let {
+            for (lineNum in startIndex until it.size) {
+                if (it[lineNum].contains("</string>")) {
+                    endIndex = lineNum
+                    break
+                }
+            }
+
+            for (lineNum in startIndex until endIndex.plus(1)) {
+                println(it[lineNum])
+            }
+        }
+        return endIndex;
+    }
+
     @Test
     @Throws(Exception::class)
     fun determine_strings_xml() {
@@ -18,40 +50,17 @@ class PrepareReleaseTest {
                 if (localeFolder.name == "values") {
                     valuesDefault = FileUtils.readLines(File(localeFolder.absolutePath + "/strings.xml"), StandardCharsets.UTF_8)
                     println("Total lines: ${valuesDefault?.size}")
-
-                    valuesDefault?.forEachIndexed { index, s ->
-                        if (s.contains("release_")) {
-                            val version = s.split("\"")[1].split("_")[1].toIntOrNull() ?: -1
-                            println("$index: ${s.split("\"")[1]} version is $version")
-                            if (version > 0 && index > startIndex) {
-                                startIndex = index
-                            }
-                        }
-                    }
-                    valuesDefault?.let {
-                        for (lineNum in startIndex until it.size) {
-                            if (it[lineNum].contains("</string>")) {
-                                endIndex = lineNum
-                                break
-                            }
-                        }
-                        println("Current release: $startIndex ~ $endIndex")
-
-                        for (lineNum in startIndex until endIndex.plus(1)) {
-                            println(it[lineNum])
-                        }
-                    }
+                    startIndex = determineLastReleaseStartLine(valuesDefault)
+                    endIndex = determineLastReleaseEndLine(valuesDefault, startIndex)
+                    println("Current release: $startIndex ~ $endIndex")
+                    println("============================================================")
                 }
 
                 localeFolder.listFiles()?.map { targetFile ->
                     if (targetFile.name == "strings.xml") {
-                        val lines = FileUtils.readLines(targetFile, StandardCharsets.UTF_8)
-                        println(targetFile.absolutePath + ": " + lines.size)
-                        lines.forEach { line ->
-//                            println(line)
-                        }
-
-
+                        val valuesOther = FileUtils.readLines(targetFile, StandardCharsets.UTF_8)
+                        println(localeFolder.name + ": " + valuesOther.size)
+                        determineLastReleaseEndLine(valuesOther, determineLastReleaseStartLine(valuesOther))
                     }
                 }
             }
