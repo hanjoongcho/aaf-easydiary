56873ba351fb2f82bfb28e5e3b6b62d9e582dced
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-03-09 17:27:26 +0900

    Update PrepareRelease package

--- app/src/main/java/me/blog/korn123/commons/utils/PrepareRelease.kt
+++ app/src/main/java/me/blog/korn123/commons/utils/PrepareRelease.kt
@@ -9,41 +9,50 @@ import java.nio.charset.StandardCharsets
 
 class PrepareRelease {
     companion object {
-        const val SYNC_RELEASE = "sync_release"
+        const val SYNC_RELEASE_STRING = "sync_release_string"
         const val SYNC_NEW_STRING = "sync_new_string"
+        const val SYNC_RELEASE_NOTE = "sync_release_note"
     }
 
-    data class Release(var currentVersion: Int = -1, var startIndex: Int = -1, var endIndex: Int = -1)
+    data class Release(var currentVersion: Int = -1, var currentVersionName: String = "", var releaseInfoLines: ArrayList<String> = arrayListOf(), var startIndex: Int = -1, var endIndex: Int = -1)
 
     fun determineLastReleaseStartLine(values: List<String>?): Release {
         val defaultRelease = Release()
-        values?.forEachIndexed { index, s ->
-            if (s.contains("release_")) {
-                val version = s.split("\"")[1].split("_")[1].toIntOrNull() ?: -1
+        var versionNameInfoIndex = -1
+        values?.let { lines ->
+            lines.forEachIndexed { index, s ->
+                if (s.contains("release_")) {
+                    val version = s.split("\"")[1].split("_")[1].toIntOrNull() ?: -1
 //                println("$index: ${s.split("\"")[1]} version is $version")
-                if (version > 0 && index > defaultRelease.startIndex) {
-                    defaultRelease.startIndex = index
-                }
+                    if (version > 0 && index > defaultRelease.startIndex) {
+                        defaultRelease.startIndex = index
+                    }
 
-                if (version > defaultRelease.currentVersion) defaultRelease.currentVersion = version
+                    if (version > defaultRelease.currentVersion) {
+                        defaultRelease.currentVersion = version
+                        versionNameInfoIndex = index.plus(1)
+                    }
+                }
             }
+            defaultRelease.currentVersionName = lines[versionNameInfoIndex].split("/")[0].trim().replace("v", "")
         }
         return defaultRelease
     }
 
-    fun determineLastReleaseEndLine(values: List<String>?, startIndex: Int, newReleaseLines: ArrayList<String>? = null): Int {
+    fun determineLastReleaseEndLine(values: List<String>?, release: Release, updateInfoLines: Boolean = false): Int {
         var endIndex = -1
         values?.let {
-            for (lineNum in startIndex until it.size) {
+            for (lineNum in release.startIndex until it.size) {
                 if (it[lineNum].contains("</string>")) {
                     endIndex = lineNum
                     break
                 }
             }
 
-            for (lineNum in startIndex until endIndex.plus(1)) {
-                println(it[lineNum])
-                newReleaseLines?.add(it[lineNum])
+            if (updateInfoLines) {
+                for (lineNum in release.startIndex until endIndex.plus(1)) {
+                    release.releaseInfoLines.add(it[lineNum])
+                }
             }
         }
         return endIndex;
@@ -54,7 +63,6 @@ class PrepareRelease {
         var valuesDefault: List<String>? = null
         var defaultRelease: Release? = null
         var defaultReleaseCurrentVersion = -1
-        val newReleaseLines: ArrayList<String> = arrayListOf()
         File("./app/src/main/res/").listFiles()?.map { localeFolder ->
             if (localeFolder.name.startsWith("values")) {
                 if (localeFolder.name == "values") {
@@ -62,32 +70,41 @@ class PrepareRelease {
                     println("Total lines: ${valuesDefault?.size}")
                     defaultRelease = determineLastReleaseStartLine(valuesDefault)
                     defaultRelease?.let {
-                        it.endIndex = determineLastReleaseEndLine(valuesDefault, it.startIndex, newReleaseLines)
-                        println("Current release: ${it.startIndex} ~ ${it.endIndex}")
+                        defaultReleaseCurrentVersion = it.currentVersion
+                        it.endIndex = determineLastReleaseEndLine(valuesDefault, it, true)
+                        println("Range of lines: ${it.startIndex} ~ ${it.endIndex}")
+                        it.releaseInfoLines.map { line ->
+                            println(line)
+                        }
+                        println("Current version: $defaultReleaseCurrentVersion")
+                        println("Current versionName: ${it.currentVersionName}")
                         println("============================================================")
                     }
-                    defaultReleaseCurrentVersion = defaultRelease?.currentVersion ?: 0
-                } else {
+                } else if (syncMode != SYNC_RELEASE_NOTE) {
                     localeFolder.listFiles()?.map { targetFile ->
                         if (targetFile.name == "strings.xml") {
                             val valuesOther = FileUtils.readLines(targetFile, StandardCharsets.UTF_8)
                             println(localeFolder.name + ": " + valuesOther.size)
                             val valuesDefaultTotal = valuesDefault?.size ?: 0
                             val otherRelease = determineLastReleaseStartLine(valuesOther)
-                            otherRelease.endIndex = determineLastReleaseEndLine(valuesOther, otherRelease.startIndex)
+                            otherRelease.endIndex = determineLastReleaseEndLine(valuesOther, otherRelease)
 
                             when {
-                                syncMode == SYNC_RELEASE && defaultReleaseCurrentVersion > otherRelease.currentVersion -> {
-                                    valuesOther.addAll(otherRelease.endIndex.plus(1), newReleaseLines)
+                                syncMode == SYNC_RELEASE_STRING && defaultReleaseCurrentVersion > otherRelease.currentVersion -> {
+                                    defaultRelease?.let {
+                                        valuesOther.addAll(otherRelease.endIndex.plus(1), it.releaseInfoLines)
+                                    }
                                 }
-                                syncMode == SYNC_RELEASE && defaultReleaseCurrentVersion == otherRelease.currentVersion -> {
+                                syncMode == SYNC_RELEASE_STRING && defaultReleaseCurrentVersion == otherRelease.currentVersion -> {
                                     println("remove line : ${otherRelease.startIndex} ~ ${otherRelease.endIndex}")
                                     for (removeLineNum in otherRelease.startIndex..otherRelease.endIndex) {
                                         valuesOther.removeAt(otherRelease.startIndex)
                                     }
-                                    valuesOther.addAll(otherRelease.startIndex, newReleaseLines)
+                                    defaultRelease?.let {
+                                        valuesOther.addAll(otherRelease.startIndex, it.releaseInfoLines)
+                                    }
                                 }
-                                else -> {
+                                syncMode == SYNC_NEW_STRING -> {
                                     for (i in valuesOther.size until valuesDefaultTotal) {
                                         valuesOther.add(valuesOther.lastIndex, valuesDefault?.get(i.minus(1)))
                                     }
@@ -101,11 +118,16 @@ class PrepareRelease {
                 }
             }
         }
+
+        if (syncMode == SYNC_RELEASE_NOTE) {
+
+        }
     }
 }
 
 fun main() {
     val prepareRelease = PrepareRelease()
-    prepareRelease.syncReleaseInformation(PrepareRelease.SYNC_RELEASE)
+//    prepareRelease.syncReleaseInformation(PrepareRelease.SYNC_RELEASE_STRING)
 //    prepareRelease.syncReleaseInformation(PrepareRelease.SYNC_NEW_STRING)
+    prepareRelease.syncReleaseInformation(PrepareRelease.SYNC_RELEASE_NOTE)
 }
