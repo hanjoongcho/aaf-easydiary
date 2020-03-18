1347917dce6f67ef57533961365839a49e280367
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-03-19 06:36:17 +0900

    Refactoring based on inspection code analyze reporting

--- app/src/main/java/me/blog/korn123/easydiary/views/SlidingTabLayout.kt
+++ app/src/main/java/me/blog/korn123/easydiary/views/SlidingTabLayout.kt
@@ -167,19 +167,19 @@ class SlidingTabLayout @JvmOverloads constructor(context: Context, attrs: Attrib
         textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, TAB_VIEW_TEXT_SIZE_SP.toFloat())
         textView.typeface = Typeface.DEFAULT_BOLD
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             // If we're running on Honeycomb or newer, then we can use the Theme's
             // selectableItemBackground to ensure that the View has a pressed state
             val outValue = TypedValue()
             getContext().theme.resolveAttribute(android.R.attr.selectableItemBackground,
                     outValue, true)
             textView.setBackgroundResource(outValue.resourceId)
-        }
+//        }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
             // If we're running on ICS or newer, enable all-caps to match the Action Bar tab style
             textView.isAllCaps = true
-        }
+//        }
 
         val padding = (TAB_VIEW_PADDING_DIPS * resources.displayMetrics.density).toInt()
         textView.setPadding(padding, padding, padding, padding)
