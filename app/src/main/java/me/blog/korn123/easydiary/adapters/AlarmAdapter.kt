82681cd5c9d5fb5024363f2500075f07706951a8
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-05-01 17:18:06 +0900

    Diary reminder feature added to development console

--- app/src/main/java/me/blog/korn123/easydiary/adapters/AlarmAdapter.kt
+++ app/src/main/java/me/blog/korn123/easydiary/adapters/AlarmAdapter.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
+import me.blog.korn123.commons.utils.FontUtils
 import me.blog.korn123.easydiary.R
+import me.blog.korn123.easydiary.extensions.initTextSize
+import me.blog.korn123.easydiary.extensions.updateAppViews
+import me.blog.korn123.easydiary.extensions.updateCardViewPolicy
+import me.blog.korn123.easydiary.extensions.updateTextColors
 import me.blog.korn123.easydiary.models.Alarm
 import me.blog.korn123.easydiary.viewholders.AlarmViewHolder
 
@@ -16,14 +21,23 @@ internal class AlarmAdapter(
 ) : RecyclerView.Adapter<AlarmViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlarmViewHolder {
-        val view = LayoutInflater.from(parent.context)
+        val view = LayoutInflater.from(activity)
                 .inflate(R.layout.viewholder_alarm, parent, false)
         return AlarmViewHolder(view, activity)
     }
 
     override fun onBindViewHolder(holder: AlarmViewHolder, position: Int) {
-        holder.itemView.setOnClickListener {
-            onItemClickListener.onItemClick(null, it, holder.adapterPosition, holder.itemId)
+        if (holder.itemView is ViewGroup) {
+            holder.itemView.run {
+                setOnClickListener {
+                    onItemClickListener.onItemClick(null, it, holder.adapterPosition, holder.itemId)
+                }
+                activity.initTextSize(this)
+                activity.updateTextColors(this)
+                activity.updateAppViews(this)
+                activity.updateCardViewPolicy(this)
+                FontUtils.setFontsTypeface(activity, activity.assets, null, this)
+            }
         }
         holder.bindTo(alarmList[position])
     }
