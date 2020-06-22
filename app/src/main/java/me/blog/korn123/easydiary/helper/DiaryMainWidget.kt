4bc2781f0d99c8812dcd7ccf71c718ad6ada473a
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-06-23 00:04:01 +0900

    Add test code to provide widget function

---
+++ app/src/main/java/me/blog/korn123/easydiary/helper/DiaryMainWidget.kt
@@ -0,0 +1,34 @@
+package me.blog.korn123.easydiary.helper
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.widget.RemoteViews
+import com.simplemobiletools.commons.helpers.isOreoPlus
+import me.blog.korn123.easydiary.R
+
+class DiaryMainWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        performUpdate(context)
+    }
+
+    private fun getComponentName(context: Context) = ComponentName(context, this::class.java)
+
+    private fun performUpdate(context: Context) {
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        appWidgetManager.getAppWidgetIds(getComponentName(context)).forEach {
+            RemoteViews(context.packageName, getProperLayout(context)).apply {
+                appWidgetManager.updateAppWidget(it, this)
+            }
+        }
+    }
+
+    private fun getProperLayout(context: Context) = if (isOreoPlus()) {
+        R.layout.simple_app_widget
+    } else {
+        R.layout.simple_app_widget
+    }
+}
