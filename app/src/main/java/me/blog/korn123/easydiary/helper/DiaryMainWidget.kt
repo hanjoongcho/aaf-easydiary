0c287bab196424d5498e9e9f03a41d9a136085d6
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-06-24 18:36:08 +0900

    Add test code to provide widget function

--- app/src/main/java/me/blog/korn123/easydiary/helper/DiaryMainWidget.kt
+++ app/src/main/java/me/blog/korn123/easydiary/helper/DiaryMainWidget.kt
@@ -1,5 +1,6 @@
 package me.blog.korn123.easydiary.helper
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
@@ -11,13 +12,33 @@ import android.net.Uri
 import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.simplemobiletools.commons.helpers.isOreoPlus
 import me.blog.korn123.easydiary.R
+import me.blog.korn123.easydiary.activities.DiaryInsertActivity
+import me.blog.korn123.easydiary.extensions.changeDrawableIconColor
+import me.blog.korn123.easydiary.extensions.config
 
 
 class DiaryMainWidget : AppWidgetProvider() {
 
+    companion object {
+        const val OPEN_WRITE_PAGE = "open_write_page"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            OPEN_WRITE_PAGE -> {
+                context.startActivity(Intent(context, DiaryInsertActivity::class.java))
+            }
+            else -> super.onReceive(context, intent)
+        }
+    }
+
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        context.run {
+            changeDrawableIconColor(config.textColor, R.drawable.edit)
+        }
         val appWidgetManager = AppWidgetManager.getInstance(context)
         appWidgetManager.getAppWidgetIds(getComponentName(context)).forEach {
             RemoteViews(context.packageName, getProperLayout(context)).apply {
@@ -33,6 +54,8 @@ class DiaryMainWidget : AppWidgetProvider() {
                     drawable.draw(c)
                     setImageViewBitmap(R.id.symbol, b)
                 }
+
+                setupIntent(context, this, OPEN_WRITE_PAGE, R.id.openWritePage)
 
                 Intent(context, DiaryMainWidgetService::class.java).apply {
                     setRemoteAdapter(R.id.diaryListView, this)
@@ -50,6 +73,14 @@ class DiaryMainWidget : AppWidgetProvider() {
 
     private fun performUpdate(context: Context) {
 
+    }
+
+    private fun setupIntent(context: Context, views: RemoteViews, action: String, id: Int) {
+        Intent(context, DiaryMainWidget::class.java).apply {
+            this.action = action
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, this, 0)
+            views.setOnClickPendingIntent(id, pendingIntent)
+        }
     }
 
     private fun getProperLayout(context: Context) = if (isOreoPlus()) {
--- app/src/main/java/me/blog/korn123/easydiary/helper/DiaryMainWidgetFactory.kt
+++ app/src/main/java/me/blog/korn123/easydiary/helper/DiaryMainWidgetFactory.kt
@@ -1,9 +1,14 @@
 package me.blog.korn123.easydiary.helper
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.os.Build
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
+import androidx.core.content.ContextCompat
 import io.github.aafactory.commons.utils.DateUtils
+import me.blog.korn123.commons.utils.FlavorUtils
 import me.blog.korn123.easydiary.R
 import me.blog.korn123.easydiary.models.DiaryDto
 
@@ -28,11 +33,28 @@ class DiaryMainWidgetFactory(private val context: Context) : RemoteViewsService.
     override fun getViewAt(position: Int): RemoteViews {
         val listViewWidget = RemoteViews(context.packageName, R.layout.widget_item_diary_main)
         val diaryDto = diaryItems[position]
-        listViewWidget.setTextViewText(R.id.text1, diaryDto.title)
-        listViewWidget.setTextViewText(R.id.text3, when (diaryDto.isAllDay) {
-            true -> DateUtils.getFullPatternDate(diaryDto.currentTimeMillis)
-            false -> DateUtils.getFullPatternDateWithTime(diaryDto.currentTimeMillis)
-        })
+
+        listViewWidget.run {
+            setTextViewText(R.id.text1, diaryDto.title)
+            setTextViewText(R.id.text2, diaryDto.contents)
+            setTextViewText(R.id.text3, when (diaryDto.isAllDay) {
+                true -> DateUtils.getFullPatternDate(diaryDto.currentTimeMillis)
+                false -> DateUtils.getFullPatternDateWithTime(diaryDto.currentTimeMillis)
+            })
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                setImageViewResource(R.id.diarySymbol, FlavorUtils.sequenceToSymbolResourceId(diaryDto.weather))
+            } else {
+                val drawable = ContextCompat.getDrawable(context, FlavorUtils.sequenceToSymbolResourceId(diaryDto.weather))
+                val b = Bitmap.createBitmap(drawable!!.intrinsicWidth,
+                        drawable.intrinsicHeight,
+                        Bitmap.Config.ARGB_8888)
+                val c = Canvas(b)
+                drawable.setBounds(0, 0, c.width, c.height)
+                drawable.draw(c)
+                setImageViewBitmap(R.id.diarySymbol, b)
+            }
+        }
 
 //        val dataIntent = Intent()
 //        dataIntent.putExtra("item_id", arrayList.get(position)._id)
