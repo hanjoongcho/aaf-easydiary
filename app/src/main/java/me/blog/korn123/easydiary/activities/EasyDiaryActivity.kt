e6e1d972129afaefc2e0a9a620567f6fc7a22ba3
hanjoong.cho <hanjoongcho@gmail.com>
2020-11-02 23:35:24 +0900

    Update release informations.

--- app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
@@ -58,6 +58,7 @@ open class EasyDiaryActivity : BaseSimpleActivity() {
 
     fun checkWhatsNewDialog(applyFilter: Boolean = true) {
         arrayListOf<Release>().apply {
+            add(Release(219, R.string.release_219))
             add(Release(218, R.string.release_218))
             add(Release(217, R.string.release_217))
             add(Release(216, R.string.release_216))
