37e85e74b90d70ec858996d48558afaf3544dd6c
hanjoong.cho <hanjoongcho@gmail.com>
2020-12-15 15:27:20 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easydiary/adapters/CaldroidItemAdapter.kt
+++ app/src/main/java/me/blog/korn123/easydiary/adapters/CaldroidItemAdapter.kt
@@ -1,15 +1,18 @@
 package me.blog.korn123.easydiary.adapters
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import com.roomorama.caldroid.CaldroidGridAdapter
 import io.realm.Sort
+import kotlinx.android.synthetic.main.fragment_custom_cell.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import me.blog.korn123.commons.utils.EasyDiaryUtils
 import me.blog.korn123.commons.utils.FlavorUtils
 import me.blog.korn123.commons.utils.FontUtils
@@ -20,12 +23,12 @@ import me.blog.korn123.easydiary.helper.CALENDAR_SORTING_ASC
 import me.blog.korn123.easydiary.helper.EasyDiaryDbHelper
 
 class CaldroidItemAdapter(
-        context: Context,
+        val activity: Activity,
         month: Int,
         year: Int,
         caldroidData: Map<String, Any?>,
         extraData: Map<String, Any>
-) : CaldroidGridAdapter(context, month, year, caldroidData, extraData) {
+) : CaldroidGridAdapter(activity, month, year, caldroidData, extraData) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
         val inflater = context
@@ -48,19 +51,24 @@ class CaldroidItemAdapter(
 
         // Get dateTime of this cell
         val dateTime = this.datetimeList[position]
-        val resources = context.resources
 
-        val calendarDate = cellView?.findViewById<TextView>(R.id.calendarDate)
-        calendarDate?.run {
-            //        calendarDate?.setBackgroundColor(Color.WHITE)
-            setBackgroundResource(0)
+        val calendarDate = cellView?.findViewById<TextView>(R.id.calendarDate)?.apply {
+            // Today's symbol
+            if (dateTime == getToday()) {
+                setBackgroundResource(R.drawable.bg_calendar_circle)
+                setTextColor(Color.WHITE)
+            } else {
+                setBackgroundResource(0)
+                when (dateTime.weekDay) {
+                    1 -> setTextColor(Color.RED)
+                    7 -> setTextColor(Color.BLUE)
+                    else -> setTextColor(Color.BLACK)
+                }
+            }
+
+            text = "${datetimeList[position].day}"
             layoutParams?.width = (textSize * 2).toInt()
             layoutParams?.height = (textSize * 2).toInt()
-        }
-        when (dateTime.weekDay) {
-            1 -> calendarDate?.setTextColor(Color.RED)
-            7 -> calendarDate?.setTextColor(Color.BLUE)
-            else -> calendarDate?.setTextColor(Color.BLACK)
         }
 
         if (dateTime.month != month) { // Set color of the dates in previous / next month
@@ -71,115 +79,113 @@ class CaldroidItemAdapter(
 
         // Customize for selected dates
         if (selectedDates != null && selectedDates.indexOf(dateTime) != -1) {
-            cellView?.let {
-                it.setBackgroundResource(R.drawable.bg_card_cell_select)
-                (it.findViewById<LinearLayout>(R.id.item1).getChildAt(1) as TextView).setTextColor(Color.BLACK)
-                (it.findViewById<LinearLayout>(R.id.item2).getChildAt(1) as TextView).setTextColor(Color.BLACK)
-                (it.findViewById<LinearLayout>(R.id.item3).getChildAt(1) as TextView).setTextColor(Color.BLACK)
+            cellView?.run {
+                setBackgroundResource(R.drawable.bg_card_cell_select)
+                (item1.getChildAt(1) as TextView).setTextColor(Color.BLACK)
+                (item2.getChildAt(1) as TextView).setTextColor(Color.BLACK)
+                (item3.getChildAt(1) as TextView).setTextColor(Color.BLACK)
             }
         } else {
-            cellView?.let {
-                it.setBackgroundColor(context.config.backgroundColor)
-                (it.findViewById<LinearLayout>(R.id.item1).getChildAt(1) as TextView).setTextColor(context.config.textColor)
-                (it.findViewById<LinearLayout>(R.id.item2).getChildAt(1) as TextView).setTextColor(context.config.textColor)
-                (it.findViewById<LinearLayout>(R.id.item3).getChildAt(1) as TextView).setTextColor(context.config.textColor)
+            cellView?.run {
+                setBackgroundColor(context.config.backgroundColor)
+                (item1.getChildAt(1) as TextView).setTextColor(context.config.textColor)
+                (item2.getChildAt(1) as TextView).setTextColor(context.config.textColor)
+                (item3.getChildAt(1) as TextView).setTextColor(context.config.textColor)
             }
         }
 
-        // Today's symbol
-        if (dateTime == getToday()) {
-            calendarDate?.setBackgroundResource(R.drawable.bg_calendar_circle)
-            calendarDate?.setTextColor(Color.WHITE)
-        }
+        cellView?.run {
 
-        calendarDate?.text = "${dateTime.day}"
+            // Somehow after setBackgroundResource, the padding collapse.
+            // This is to recover the padding
+            setPadding(leftPadding, topPadding, rightPadding, bottomPadding)
 
-        val dateString = dateTime.format("YYYY-MM-DD")
-        val count = EasyDiaryDbHelper.countDiaryBy(dateString)
+            GlobalScope.launch {
+                activity.runOnUiThread {
+                    val dateString = dateTime.format("YYYY-MM-DD")
+                    val count = EasyDiaryDbHelper.countDiaryBy(dateString)
 
-        val sort: Sort = if (context.config.calendarSorting == CALENDAR_SORTING_ASC) Sort.ASCENDING else Sort.DESCENDING
-        val mDiaryList = EasyDiaryDbHelper.readDiaryByDateString(dateString, sort)
-        cellView?.findViewById<TextView>(R.id.itemCount)?.run {
-            setTextColor(Color.RED)
-            if (count > 3) {
-                text = context.getString(R.string.diary_item_count, count - 3)
-            } else {
-                text = null
+                    val sort: Sort = if (context.config.calendarSorting == CALENDAR_SORTING_ASC) Sort.ASCENDING else Sort.DESCENDING
+                    val mDiaryList = EasyDiaryDbHelper.readDiaryByDateString(dateString, sort)
+                    findViewById<TextView>(R.id.itemCount)?.run {
+                        setTextColor(Color.RED)
+                        text = if (count > 3) {
+                            context.getString(R.string.diary_item_count, count - 3)
+                        } else {
+                            null
+                        }
+                    }
+
+                    when {
+                        mDiaryList.isEmpty() -> {
+                            item1.run {
+                                (getChildAt(0) as ImageView).setImageResource(0)
+                                (getChildAt(1) as TextView).text = null
+                            }
+                            item2.run {
+                                (getChildAt(0) as ImageView).setImageResource(0)
+                                (getChildAt(1) as TextView).text = null
+                            }
+                            item3.run {
+                                (getChildAt(0) as ImageView).setImageResource(0)
+                                (getChildAt(1) as TextView).text = null
+                            }
+                        }
+                        mDiaryList.size == 1 -> {
+                            item1.run {
+                                val item = mDiaryList[0]
+                                FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
+                                (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
+                            }
+                            item2.run {
+                                (getChildAt(0) as ImageView).setImageResource(0)
+                                (getChildAt(1) as TextView).text = null
+                            }
+                            item3.run {
+                                (getChildAt(0) as ImageView).setImageResource(0)
+                                (getChildAt(1) as TextView).text = null
+                            }
+                        }
+                        mDiaryList.size == 2 -> {
+                            item1.run {
+                                val item = mDiaryList[0]
+                                FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
+                                (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
+                            }
+                            item2.run {
+                                val item = mDiaryList[1]
+                                FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
+                                (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
+                            }
+                            item3.run {
+                                (getChildAt(0) as ImageView).setImageResource(0)
+                                (getChildAt(1) as TextView).text = null
+                            }
+                        }
+                        mDiaryList.size > 2 -> {
+                            item1.run {
+                                val item = mDiaryList[0]
+                                FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
+                                (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
+                            }
+                            item2.run {
+                                val item = mDiaryList[1]
+                                FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
+                                (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
+                            }
+                            item3.run {
+                                val item = mDiaryList[2]
+                                FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
+                                (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
+                            }
+                        }
+                    }
+
+                    // Set custom color if required
+                    setCustomResources(dateTime, cellView, calendarDate)
+                }
             }
         }
-
-        when {
-            mDiaryList.isEmpty() -> {
-                cellView?.findViewById<LinearLayout>(R.id.item1)?.run {
-                    (getChildAt(0) as ImageView).setImageResource(0)
-                    (getChildAt(1) as TextView).text = null
-                }
-                cellView?.findViewById<LinearLayout>(R.id.item2)?.run {
-                    (getChildAt(0) as ImageView).setImageResource(0)
-                    (getChildAt(1) as TextView).text = null
-                }
-                cellView?.findViewById<LinearLayout>(R.id.item3)?.run {
-                    (getChildAt(0) as ImageView).setImageResource(0)
-                    (getChildAt(1) as TextView).text = null
-                }
-            }
-            mDiaryList.size == 1 -> {
-                cellView?.findViewById<LinearLayout>(R.id.item1)?.run {
-                    val item = mDiaryList[0]
-                    FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
-                    (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
-                }
-                cellView?.findViewById<LinearLayout>(R.id.item2)?.run {
-                    (getChildAt(0) as ImageView).setImageResource(0)
-                    (getChildAt(1) as TextView).text = null
-                }
-                cellView?.findViewById<LinearLayout>(R.id.item3)?.run {
-                    (getChildAt(0) as ImageView).setImageResource(0)
-                    (getChildAt(1) as TextView).text = null
-                }
-            }
-            mDiaryList.size == 2 -> {
-                cellView?.findViewById<LinearLayout>(R.id.item1)?.run {
-                    val item = mDiaryList[0]
-                    FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
-                    (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
-                }
-                cellView?.findViewById<LinearLayout>(R.id.item2)?.run {
-                    val item = mDiaryList[1]
-                    FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
-                    (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
-                }
-                cellView?.findViewById<LinearLayout>(R.id.item3)?.run {
-                    (getChildAt(0) as ImageView).setImageResource(0)
-                    (getChildAt(1) as TextView).text = null
-                }
-            }
-            mDiaryList.size > 2 -> {
-                cellView?.findViewById<LinearLayout>(R.id.item1)?.run {
-                    val item = mDiaryList[0]
-                    FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
-                    (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
-                }
-                cellView?.findViewById<LinearLayout>(R.id.item2)?.run {
-                    val item = mDiaryList[1]
-                    FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
-                    (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
-                }
-                cellView?.findViewById<LinearLayout>(R.id.item3)?.run {
-                    val item = mDiaryList[2]
-                    FlavorUtils.initWeatherView(context, getChildAt(0) as ImageView, item.weather)
-                    (getChildAt(1) as TextView).text = EasyDiaryUtils.summaryDiaryLabel(item)
-                }
-            }
-        }
-
-        // Somehow after setBackgroundResource, the padding collapse.
-        // This is to recover the padding
-        cellView?.setPadding(leftPadding, topPadding, rightPadding,
-                bottomPadding)
-
-        // Set custom color if required
-        setCustomResources(dateTime, cellView, calendarDate)
         return cellView
     }
 }
