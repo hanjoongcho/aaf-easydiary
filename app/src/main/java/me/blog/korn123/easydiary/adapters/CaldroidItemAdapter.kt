49e342e415b9e0884a14b66dd7904f7faaea0c74
hanjoong.cho <hanjoongcho@gmail.com>
2021-01-01 16:56:47 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easydiary/adapters/CaldroidItemAdapter.kt
+++ app/src/main/java/me/blog/korn123/easydiary/adapters/CaldroidItemAdapter.kt
@@ -1,26 +1,22 @@
 package me.blog.korn123.easydiary.adapters
 
 import android.app.Activity
-import android.content.Context
 import android.graphics.Color
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.roomorama.caldroid.CaldroidGridAdapter
 import io.realm.Sort
-import kotlinx.android.synthetic.main.fragment_custom_cell.view.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import me.blog.korn123.commons.utils.EasyDiaryUtils
 import me.blog.korn123.commons.utils.FlavorUtils
 import me.blog.korn123.commons.utils.FontUtils
 import me.blog.korn123.easydiary.R
+import me.blog.korn123.easydiary.databinding.FragmentCustomCellBinding
 import me.blog.korn123.easydiary.extensions.config
 import me.blog.korn123.easydiary.extensions.initTextSize
-import me.blog.korn123.easydiary.helper.AAF_TEST
 import me.blog.korn123.easydiary.helper.CALENDAR_SORTING_ASC
 import me.blog.korn123.easydiary.helper.EasyDiaryDbHelper
 
@@ -33,23 +29,22 @@ class CaldroidItemAdapter(
 ) : CaldroidGridAdapter(activity, month, year, caldroidData, extraData) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        var cellView = convertView
-
-        if (convertView == null) {
-            cellView = inflater.inflate(R.layout.fragment_custom_cell, null).apply {
-                if (this is ViewGroup) {
-                    context?.initTextSize(this)
-                    FontUtils.setFontsTypeface(context, context.assets, null, this)
-                    setBackgroundColor(context.config.backgroundColor)
-                }
+        val itemView: View = convertView ?: run {
+            val binding = FragmentCustomCellBinding.inflate(activity.layoutInflater)
+            binding.root.apply {
+                tag = binding
+                context?.initTextSize(this)
+                FontUtils.setFontsTypeface(context, context.assets, null, this)
+                setBackgroundColor(context.config.backgroundColor)
             }
         }
+
+        val binding = itemView.tag as FragmentCustomCellBinding
 
         // Get dateTime of this cell
         val dateTime = this.datetimeList[position]
 
-        val calendarDate = cellView?.findViewById<TextView>(R.id.calendarDate)?.apply {
+        val calendarDate = binding.calendarDate.apply {
             // Today's symbol
             if (dateTime == getToday()) {
                 setBackgroundResource(R.drawable.bg_calendar_circle)
@@ -69,31 +64,29 @@ class CaldroidItemAdapter(
         }
 
         if (dateTime.month != month) { // Set color of the dates in previous / next month
-            calendarDate?.alpha = 0.5F
+            calendarDate.alpha = 0.5F
         } else {
-            calendarDate?.alpha = 1.0F
+            calendarDate.alpha = 1.0F
         }
 
-        cellView?.run {
+        binding.run {
             GlobalScope.launch {
-                Log.i(AAF_TEST, "current thread: ${Thread.currentThread().name}")
-
                 activity.runOnUiThread {
                     val dateString = dateTime.format("YYYY-MM-DD")
                     val count = EasyDiaryDbHelper.countDiaryBy(dateString)
-                    val topPadding = paddingTop ?: 0
-                    val leftPadding = paddingLeft ?: 0
-                    val bottomPadding = paddingBottom ?: 0
-                    val rightPadding = paddingRight ?: 0
+                    val topPadding = root.paddingTop
+                    val leftPadding = root.paddingLeft
+                    val bottomPadding = root.paddingBottom
+                    val rightPadding = root.paddingRight
 
                     // Customize for selected dates
                     if (selectedDates != null && selectedDates.indexOf(dateTime) != -1) {
-                        setBackgroundResource(R.drawable.bg_card_cell_select)
+                        root.setBackgroundResource(R.drawable.bg_card_cell_select)
                         (item1.getChildAt(1) as TextView).setTextColor(Color.BLACK)
                         (item2.getChildAt(1) as TextView).setTextColor(Color.BLACK)
                         (item3.getChildAt(1) as TextView).setTextColor(Color.BLACK)
                     } else {
-                        setBackgroundColor(context.config.backgroundColor)
+                        root.setBackgroundColor(context.config.backgroundColor)
                         (item1.getChildAt(1) as TextView).setTextColor(context.config.textColor)
                         (item2.getChildAt(1) as TextView).setTextColor(context.config.textColor)
                         (item3.getChildAt(1) as TextView).setTextColor(context.config.textColor)
@@ -101,11 +94,11 @@ class CaldroidItemAdapter(
 
                     // Somehow after setBackgroundResource, the padding collapse.
                     // This is to recover the padding
-                    setPadding(leftPadding, topPadding, rightPadding, bottomPadding)
+                    root.setPadding(leftPadding, topPadding, rightPadding, bottomPadding)
 
                     val sort: Sort = if (context.config.calendarSorting == CALENDAR_SORTING_ASC) Sort.ASCENDING else Sort.DESCENDING
                     val mDiaryList = EasyDiaryDbHelper.readDiaryByDateString(dateString, sort)
-                    findViewById<TextView>(R.id.itemCount)?.run {
+                    itemCount.run {
                         setTextColor(Color.RED)
                         text = if (count > 3) {
                             context.getString(R.string.diary_item_count, count - 3)
@@ -180,10 +173,10 @@ class CaldroidItemAdapter(
                     }
 
                     // Set custom color if required
-                    setCustomResources(dateTime, cellView, calendarDate)
+                    setCustomResources(dateTime, itemView, calendarDate)
                 }
             }
         }
-        return cellView
+        return itemView
     }
 }
