3eac6f950da440db3b4314c54b252d7902996c15
Bulbasaur <hanjoongcho@gmail.com>
2021-06-06 22:06:01 +0900

    Added code for testing

--- app/src/main/java/me/blog/korn123/easydiary/activities/DiaryMainActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/DiaryMainActivity.kt
@@ -149,6 +149,12 @@ class DiaryMainActivity : ToolbarControlBaseActivity<ObservableListView>() {
             if (task.isSuccessful) {
                 // We got the ReviewInfo object
                 val reviewInfo = task.result
+                val flow = manager.launchReviewFlow(this, reviewInfo)
+                flow.addOnCompleteListener { _ ->
+                    // The flow has finished. The API does not indicate whether the user
+                    // reviewed or not, or even whether the review dialog was shown. Thus, no
+                    // matter the result, we continue our app flow.
+                }
             } else {
                 // There was some problem, log or handle the error code.
 //                @ReviewErrorCode val reviewErrorCode = (task.getException() as TaskException).errorCode
