5991af9ccc0068084d40a05d7883a45efffa68e5
Bulbasaur <hanjoongcho@gmail.com>
2021-07-12 00:29:42 +0900

    Changed deprecated api to new api

--- app/src/main/java/me/blog/korn123/easydiary/fragments/PhotoFlexItemOptionFragment.kt
+++ app/src/main/java/me/blog/korn123/easydiary/fragments/PhotoFlexItemOptionFragment.kt
@@ -8,13 +8,13 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.synthetic.main.dialog_postcard_photo_option.*
 import me.blog.korn123.easydiary.R
 import me.blog.korn123.easydiary.adapters.PhotoAdapter
 import me.blog.korn123.easydiary.adapters.PhotoAdapter.PhotoViewHolder.Companion.applyOption
+import me.blog.korn123.easydiary.databinding.DialogPostcardPhotoOptionBinding
 
 class PhotoFlexItemOptionFragment : DialogFragment() {
-
+    private lateinit var mBinding: DialogPostcardPhotoOptionBinding
     private var itemIndex: Int = -1
     private var viewMode = 0
     private var filterMode = 0
@@ -40,51 +40,53 @@ class PhotoFlexItemOptionFragment : DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dialog?.setTitle(getString(R.string.title_dialog_postcard_photo_option))
-        return inflater.inflate(R.layout.dialog_postcard_photo_option, container, false)
+        mBinding = DialogPostcardPhotoOptionBinding.inflate(layoutInflater)
+        return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mBinding.run {
+            spinnerViewMode.run {
+                val arrayAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.options_spinner_view_mode, R.layout.item_spinner)
+                adapter = arrayAdapter
+                setSelection(viewMode)
 
-        spinner_view_mode.run {
-            val arrayAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.options_spinner_view_mode, R.layout.item_spinner)
-            adapter = arrayAdapter
-            setSelection(viewMode)
+                onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                        viewMode = position
+                        applyOption(requireContext(), photoUri!!, viewMode, filterMode, imagePreview)
+                    }
 
-            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
-                    viewMode = position
-                    applyOption(requireContext(), photoUri!!, viewMode, filterMode, image_preview)
+                    override fun onNothingSelected(p0: AdapterView<*>?) {}
                 }
-
-                override fun onNothingSelected(p0: AdapterView<*>?) {}
             }
-        }
 
-        applyOption(requireContext(), photoUri!!, viewMode, filterMode, image_preview)
+            applyOption(requireContext(), photoUri!!, viewMode, filterMode, imagePreview)
 
-        spinner_filter_mode.run {
-            val arrayAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.options_spinner_filter_mode, R.layout.item_spinner)
-            adapter = arrayAdapter
-            setSelection(filterMode)
+            spinnerFilterMode.run {
+                val arrayAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.options_spinner_filter_mode, R.layout.item_spinner)
+                adapter = arrayAdapter
+                setSelection(filterMode)
 
-            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
-                    filterMode = position
-                    applyOption(requireContext(), photoUri!!, viewMode, filterMode, image_preview)
+                onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                        filterMode = position
+                        applyOption(requireContext(), photoUri!!, viewMode, filterMode, imagePreview)
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {}
                 }
-
-                override fun onNothingSelected(p0: AdapterView<*>?) {}
             }
-        }
 
-        check_force_single_photo.isChecked = forceSinglePhotoPosition
+            checkForceSinglePhoto.isChecked = forceSinglePhotoPosition
 
-        button_ok.setOnClickListener {
-            dismiss()
-            positiveCallback.invoke(spinner_view_mode.selectedItemPosition, spinner_filter_mode.selectedItemPosition, check_force_single_photo.isChecked)
+            buttonOk.setOnClickListener {
+                dismiss()
+                positiveCallback.invoke(spinnerViewMode.selectedItemPosition, spinnerFilterMode.selectedItemPosition, checkForceSinglePhoto.isChecked)
+            }
+            buttonCancel.setOnClickListener { dismiss() }
         }
-        button_cancel.setOnClickListener { dismiss() }
     }
 
     companion object {
