f89a6da2f3372be18aa236bd44bd29c048a3958a
Bulbasaur <hanjoongcho@gmail.com>
2021-11-13 09:30:23 +0900

    Update release informations

--- app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
@@ -59,6 +59,7 @@ open class EasyDiaryActivity : BaseSimpleActivity() {
 
     fun checkWhatsNewDialog(applyFilter: Boolean = true) {
         arrayListOf<Release>().apply {
+            add(Release(251, R.string.release_251))
             add(Release(250, R.string.release_250))
             add(Release(246, R.string.release_246))
             add(Release(244, R.string.release_244))
