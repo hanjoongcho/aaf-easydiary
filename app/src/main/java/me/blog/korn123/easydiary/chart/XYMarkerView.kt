4161db9ba081d8465852344752541a41e4c70473
Bulbasaur <hanjoongcho@gmail.com>
2022-03-29 02:12:36 +0900

    Statistical chart feature improved

--- app/src/main/java/me/blog/korn123/easydiary/chart/XYMarkerView.kt
+++ app/src/main/java/me/blog/korn123/easydiary/chart/XYMarkerView.kt
@@ -2,8 +2,10 @@ package me.blog.korn123.easydiary.chart
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.widget.ImageView
 import android.widget.TextView
 import com.github.mikephil.charting.components.MarkerView
+import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import com.github.mikephil.charting.highlight.Highlight
@@ -19,6 +21,7 @@ import me.blog.korn123.easydiary.R
  */
 class XYMarkerView(context: Context, private val xAxisValueFormatter: IAxisValueFormatter) : MarkerView(context, R.layout.partial_custom_marker_view) {
     private val tvContent: TextView = findViewById(R.id.tvContent)
+    private val imageDiarySymbol: ImageView = findViewById(R.id.image_diary_symbol)
 
     // callbacks everytime the MarkerView is redrawn, can be used to update the
     // content (user-interface)
@@ -27,6 +30,10 @@ class XYMarkerView(context: Context, private val xAxisValueFormatter: IAxisValue
             tvContent.run {
                 text = String.format("%s: %d", context.getString(R.string.write_count), entry.y.toInt())
                 typeface = FontUtils.getCommonTypeface(context)
+            }
+            entry.icon?.let {
+                imageDiarySymbol.visibility = VISIBLE
+                imageDiarySymbol.setImageDrawable(it)
             }
             super.refreshContent(entry, highlight)
         }
