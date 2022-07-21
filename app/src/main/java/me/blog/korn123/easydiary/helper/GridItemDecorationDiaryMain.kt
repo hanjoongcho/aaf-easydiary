7406047c6833798e3918e8aae0ca567a90640212
Bulbasaur <hanjoongcho@gmail.com>
2022-07-22 07:20:57 +0900

    WIP

--- app/src/main/java/me/blog/korn123/easydiary/helper/GridItemDecorationDiaryMain.kt
+++ app/src/main/java/me/blog/korn123/easydiary/helper/GridItemDecorationDiaryMain.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.graphics.Rect
 import android.view.View
 import me.blog.korn123.easydiary.extensions.diaryMainSpanCount
+import me.blog.korn123.easydiary.extensions.dpToPixel
 
 class GridItemDecorationDiaryMain(private val space: Int, private val activity: Activity) : androidx.recyclerview.widget.RecyclerView.ItemDecoration() {
     override fun getItemOffsets(outRect: Rect, view: View, parent: androidx.recyclerview.widget.RecyclerView, state: androidx.recyclerview.widget.RecyclerView.State) {
@@ -13,5 +14,8 @@ class GridItemDecorationDiaryMain(private val space: Int, private val activity: 
         val isStartColumn = position % spanCount == 0
         outRect.top = if (isTopColumn) 0 else space
         outRect.left = if (isStartColumn) 0 else space
+        parent.adapter?.run {
+            outRect.bottom = if (position == itemCount.minus(1)) activity.dpToPixel(74F) else 0
+        }
     }
 }
