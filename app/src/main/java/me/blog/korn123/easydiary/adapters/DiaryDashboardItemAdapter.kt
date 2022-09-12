27ed390def020ab50ae69a8cbd63e003e65c7aa5
Bulbasaur <hanjoongcho@gmail.com>
2022-09-13 06:34:09 +0900

    [WIP] Add the ability to view diaries by category on the dashboard

--- app/src/main/java/me/blog/korn123/easydiary/adapters/DiaryDashboardItemAdapter.kt
+++ app/src/main/java/me/blog/korn123/easydiary/adapters/DiaryDashboardItemAdapter.kt
@@ -1,145 +1,181 @@
 package me.blog.korn123.easydiary.adapters
 
 import android.app.Activity
-import android.graphics.drawable.Drawable
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
+import com.zhpan.bannerview.BaseBannerAdapter
+import com.zhpan.bannerview.BaseViewHolder
 import me.blog.korn123.commons.utils.DateUtils
-import me.blog.korn123.commons.utils.EasyDiaryUtils
-import me.blog.korn123.commons.utils.EasyDiaryUtils.createBackgroundGradientDrawable
-import me.blog.korn123.commons.utils.EasyDiaryUtils.createThumbnailGlideOptions
-import me.blog.korn123.commons.utils.FlavorUtils
 import me.blog.korn123.commons.utils.FontUtils
 import me.blog.korn123.easydiary.R
 import me.blog.korn123.easydiary.databinding.ItemDiaryDashboardBinding
-import me.blog.korn123.easydiary.enums.Calculation
 import me.blog.korn123.easydiary.extensions.*
-import me.blog.korn123.easydiary.helper.PHOTO_CORNER_RADIUS_SCALE_FACTOR_NORMAL
-import me.blog.korn123.easydiary.helper.THUMBNAIL_BACKGROUND_ALPHA
 import me.blog.korn123.easydiary.models.Diary
 
-class DiaryDashboardItemAdapter(
-        val activity: Activity,
-        private val diaryItems: List<Diary>,
-        val itemClickCallback: (diary: Diary) -> Unit,
-        val itemLongClickCallback: () -> Unit
-) : RecyclerView.Adapter<DiaryDashboardItemAdapter.ViewHolder>() {
+class DiaryDashboardItemAdapter : BaseBannerAdapter<Diary>() {
     var currentQuery: String? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(activity, ItemDiaryDashboardBinding.inflate(activity.layoutInflater, parent, false), this)
-    }
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        return ViewHolder(activity, ItemDiaryDashboardBinding.inflate(activity.layoutInflater, parent, false), this)
+//    }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindTo(diaryItems[position])
-    }
-
-    override fun getItemCount(): Int = diaryItems.size
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        holder.bindTo(diaryItems[position])
+//    }
+//
+//    override fun getItemCount(): Int = diaryItems.size
 
     inner class ViewHolder(
             val activity: Activity, private val itemDiaryDashboardBinding: ItemDiaryDashboardBinding, val adapter: DiaryDashboardItemAdapter
     ) : RecyclerView.ViewHolder(itemDiaryDashboardBinding.root) {
-        fun bindTo(diary: Diary) {
-            itemDiaryDashboardBinding.run {
-                activity.run {
-                    root.run {
-                        setOnClickListener { itemClickCallback.invoke(diary) }
-                        setOnLongClickListener {
-                            itemLongClickCallback()
-                            true
-                        }
-                        updateTextColors(this, 0, 0)
-                        updateAppViews(this)
-                        initTextSize(this)
-                        updateCardViewPolicy(this)
-                        FontUtils.setFontsTypeface(context, null, this)
+//        fun bindTo(diary: Diary) {
+//            itemDiaryDashboardBinding.run {
+//                activity.run {
+//                    root.run {
+//                        setOnClickListener { itemClickCallback.invoke(diary) }
+//                        setOnLongClickListener {
+//                            itemLongClickCallback()
+//                            true
+//                        }
+//                        updateTextColors(this, 0, 0)
+//                        updateAppViews(this)
+//                        initTextSize(this)
+//                        updateCardViewPolicy(this)
+//                        FontUtils.setFontsTypeface(context, null, this)
+//                    }
+//
+//                    if (config.enableLocationInfo) {
+//                        diary.location?.let {
+//                            changeDrawableIconColor(config.primaryColor, R.drawable.ic_map_marker_2)
+////                        locationLabel.setTextColor(config.textColor)
+////                        locationContainer.background = getLabelBackground()
+//
+//                            locationLabel.text = it.address
+//                            locationContainer.visibility = View.VISIBLE
+//                        } ?: { locationContainer.visibility = View.GONE } ()
+//                    } else {
+//                        locationContainer.visibility = View.GONE
+//                    }
+//
+//                    if (config.enableCountCharacters) {
+//                        contentsLength.run {
+////                        setTextColor(config.textColor)
+////                        background = getLabelBackground()
+//
+//                            text = context.getString(R.string.diary_contents_length, diary.contents?.length ?: 0)
+//                        }
+//                        contentsLengthContainer.visibility = View.VISIBLE
+//                    } else {
+//                        contentsLengthContainer.visibility = View.GONE
+//                    }
+//                }
+//
+////                textContents.text = if (diary.contents?.length ?: 0 > 10) "${diary.contents?.substring(0, 10)}..." else diary.contents
+//                textContents.text = diary.contents
+//                textDateTime.text = when (diary.isAllDay) {
+//                    true -> DateUtils.getDateStringFromTimeMillis(diary.currentTimeMillis)
+//                    false -> DateUtils.getDateTimeStringFromTimeMillis(diary.currentTimeMillis)
+//                }
+//                FlavorUtils.initWeatherView(activity, imageSymbol, diary.weather)
+//
+//                when (diary.photoUris?.size ?: 0 > 0) {
+//                    true -> {
+//                        photoViews.visibility = View.VISIBLE
+//                    }
+//                    false -> photoViews.visibility = View.GONE
+//                }
+//
+//                photoViews.removeAllViews()
+//                if (diary.photoUris?.size ?: 0 > 0) {
+//                    diary.photoUrisWithEncryptionPolicy()?.map {
+//                        val imageXY = activity.dpToPixel(32F)
+//                        val imageView = ImageView(activity)
+//                        val layoutParams = LinearLayout.LayoutParams(imageXY, imageXY)
+//                        layoutParams.setMargins(0, activity.dpToPixel(1F), activity.dpToPixel(3F), 0)
+//                        imageView.layoutParams = layoutParams
+//                        imageView.background = createBackgroundGradientDrawable(activity.config.primaryColor, THUMBNAIL_BACKGROUND_ALPHA, imageXY * PHOTO_CORNER_RADIUS_SCALE_FACTOR_NORMAL)
+//                        imageView.scaleType = ImageView.ScaleType.CENTER
+//                        activity.dpToPixel(1.5F, Calculation.FLOOR).apply {
+//                            imageView.setPadding(this, this, this, this)
+//                        }
+//                        val listener = object : RequestListener<Drawable> {
+//                            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+//                                return false
+//                            }
+//
+//                            override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+//                                return false
+//                            }
+//                        }
+//                        Glide.with(activity).load(EasyDiaryUtils.getApplicationDataDirectory(activity) + it.getFilePath())
+//                            .listener(listener)
+//                            .apply(createThumbnailGlideOptions(imageXY * PHOTO_CORNER_RADIUS_SCALE_FACTOR_NORMAL, it.isEncrypt()))
+//                            .into(imageView)
+////                    if (photoViews.childCount >= maxPhotos) return@map
+//                        photoViews.addView(imageView)
+//                    }
+//                }
+//
+//                textContents.maxLines = when (activity.config.enableContentsSummary) {
+//                    true -> activity.config.summaryMaxLines
+//                    false -> Integer.MAX_VALUE
+//                }
+//            }
+//        }
+    }
+
+    override fun bindData(
+        holder: BaseViewHolder<Diary>,
+        diary: Diary,
+        position: Int,
+        pageSize: Int
+    ) {
+        val context = holder.itemView.context
+        val binding = ItemDiaryDashboardBinding.bind(holder.itemView)
+
+        binding.root.run {
+            context.updateTextColors(this, 0, 0)
+            context.updateAppViews(this)
+            context.initTextSize(this)
+            context.updateCardViewPolicy(this)
+            FontUtils.setFontsTypeface(context, null, this)
+        }
+        binding.run {
+            textContents.text = diary.contents
+            textDateTime.text = when (diary.isAllDay) {
+                true -> DateUtils.getDateStringFromTimeMillis(diary.currentTimeMillis)
+                false -> DateUtils.getDateTimeStringFromTimeMillis(diary.currentTimeMillis)
+            }
+
+            context.run {
+                if (config.enableLocationInfo) {
+                    diary.location?.let {
+                        changeDrawableIconColor(config.primaryColor, R.drawable.ic_map_marker_2)
+                        locationLabel.text = it.address
+                        locationContainer.visibility = View.VISIBLE
+                    } ?: { locationContainer.visibility = View.GONE } ()
+                } else {
+                    locationContainer.visibility = View.GONE
+                }
+                if (config.enableCountCharacters) {
+                    contentsLength.run {
+                        text = context.getString(R.string.diary_contents_length, diary.contents?.length ?: 0)
                     }
-
-                    if (config.enableLocationInfo) {
-                        diary.location?.let {
-                            changeDrawableIconColor(config.primaryColor, R.drawable.ic_map_marker_2)
-//                        locationLabel.setTextColor(config.textColor)
-//                        locationContainer.background = getLabelBackground()
-
-                            locationLabel.text = it.address
-                            locationContainer.visibility = View.VISIBLE
-                        } ?: { locationContainer.visibility = View.GONE } ()
-                    } else {
-                        locationContainer.visibility = View.GONE
-                    }
-
-                    if (config.enableCountCharacters) {
-                        contentsLength.run {
-//                        setTextColor(config.textColor)
-//                        background = getLabelBackground()
-
-                            text = context.getString(R.string.diary_contents_length, diary.contents?.length ?: 0)
-                        }
-                        contentsLengthContainer.visibility = View.VISIBLE
-                    } else {
-                        contentsLengthContainer.visibility = View.GONE
-                    }
+                    contentsLengthContainer.visibility = View.VISIBLE
+                } else {
+                    contentsLengthContainer.visibility = View.GONE
                 }
 
-//                textContents.text = if (diary.contents?.length ?: 0 > 10) "${diary.contents?.substring(0, 10)}..." else diary.contents
-                textContents.text = diary.contents
-                textDateTime.text = when (diary.isAllDay) {
-                    true -> DateUtils.getDateStringFromTimeMillis(diary.currentTimeMillis)
-                    false -> DateUtils.getDateTimeStringFromTimeMillis(diary.currentTimeMillis)
-                }
-                FlavorUtils.initWeatherView(activity, imageSymbol, diary.weather)
-
-                when (diary.photoUris?.size ?: 0 > 0) {
-                    true -> {
-                        photoViews.visibility = View.VISIBLE
-                    }
-                    false -> photoViews.visibility = View.GONE
-                }
-
-                photoViews.removeAllViews()
-                if (diary.photoUris?.size ?: 0 > 0) {
-                    diary.photoUrisWithEncryptionPolicy()?.map {
-                        val imageXY = activity.dpToPixel(32F)
-                        val imageView = ImageView(activity)
-                        val layoutParams = LinearLayout.LayoutParams(imageXY, imageXY)
-                        layoutParams.setMargins(0, activity.dpToPixel(1F), activity.dpToPixel(3F), 0)
-                        imageView.layoutParams = layoutParams
-                        imageView.background = createBackgroundGradientDrawable(activity.config.primaryColor, THUMBNAIL_BACKGROUND_ALPHA, imageXY * PHOTO_CORNER_RADIUS_SCALE_FACTOR_NORMAL)
-                        imageView.scaleType = ImageView.ScaleType.CENTER
-                        activity.dpToPixel(1.5F, Calculation.FLOOR).apply {
-                            imageView.setPadding(this, this, this, this)
-                        }
-                        val listener = object : RequestListener<Drawable> {
-                            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                                return false
-                            }
-
-                            override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                                return false
-                            }
-                        }
-                        Glide.with(activity).load(EasyDiaryUtils.getApplicationDataDirectory(activity) + it.getFilePath())
-                            .listener(listener)
-                            .apply(createThumbnailGlideOptions(imageXY * PHOTO_CORNER_RADIUS_SCALE_FACTOR_NORMAL, it.isEncrypt()))
-                            .into(imageView)
-//                    if (photoViews.childCount >= maxPhotos) return@map
-                        photoViews.addView(imageView)
-                    }
-                }
-
-                textContents.maxLines = when (activity.config.enableContentsSummary) {
-                    true -> activity.config.summaryMaxLines
+                textContents.maxLines = when (config.enableContentsSummary) {
+                    true -> config.summaryMaxLines
                     false -> Integer.MAX_VALUE
                 }
             }
         }
     }
+
+    override fun getLayoutId(viewType: Int): Int {
+        return R.layout.item_diary_dashboard
+    }
+
 }
--- app/src/main/java/me/blog/korn123/easydiary/fragments/DiaryFragment.kt
+++ app/src/main/java/me/blog/korn123/easydiary/fragments/DiaryFragment.kt
@@ -1,19 +1,23 @@
 package me.blog.korn123.easydiary.fragments
 
-import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.zhpan.bannerview.BannerViewPager
+import com.zhpan.bannerview.constants.IndicatorGravity
+import com.zhpan.bannerview.constants.PageStyle
 import me.blog.korn123.easydiary.R
-import me.blog.korn123.easydiary.activities.DiaryReadingActivity
 import me.blog.korn123.easydiary.adapters.DiaryDashboardItemAdapter
 import me.blog.korn123.easydiary.databinding.FragmentDiaryBinding
 import me.blog.korn123.easydiary.extensions.config
-import me.blog.korn123.easydiary.helper.*
+import me.blog.korn123.easydiary.extensions.dpToPixel
+import me.blog.korn123.easydiary.helper.EasyDiaryDbHelper
 import me.blog.korn123.easydiary.models.Diary
+import me.blog.korn123.easydiary.views.FigureIndicatorView
 
 class DiaryFragment : Fragment() {
 
@@ -22,7 +26,7 @@ class DiaryFragment : Fragment() {
      *
      ***************************************************************************************************/
     private lateinit var mBinding: FragmentDiaryBinding
-    private var mDiaryDashboardItemAdapter: DiaryDashboardItemAdapter? = null
+    private lateinit var mBannerDiary: BannerViewPager<Diary>
     private var mDiaryList: ArrayList<Diary> = arrayListOf()
 
 
@@ -42,40 +46,72 @@ class DiaryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mDiaryDashboardItemAdapter = DiaryDashboardItemAdapter(requireActivity(), mDiaryList, {
-            val detailIntent = Intent(requireContext(), DiaryReadingActivity::class.java)
-            detailIntent.putExtra(DIARY_SEQUENCE, it.sequence)
-            detailIntent.putExtra(SELECTED_SEARCH_QUERY, mDiaryDashboardItemAdapter?.currentQuery)
-            detailIntent.putExtra(SELECTED_SYMBOL_SEQUENCE, 0)
-            TransitionHelper.startActivityWithTransition(requireActivity(), detailIntent)
-        }, {})
+//        mDiaryDashboardItemAdapter = DiaryDashboardItemAdapter(requireActivity(), mDiaryList, {
+//            val detailIntent = Intent(requireContext(), DiaryReadingActivity::class.java)
+//            detailIntent.putExtra(DIARY_SEQUENCE, it.sequence)
+//            detailIntent.putExtra(SELECTED_SEARCH_QUERY, mDiaryDashboardItemAdapter?.currentQuery)
+//            detailIntent.putExtra(SELECTED_SYMBOL_SEQUENCE, 0)
+//            TransitionHelper.startActivityWithTransition(requireActivity(), detailIntent)
+//        }, {})
 
-        mBinding.recyclerDiary.run {
-            adapter = mDiaryDashboardItemAdapter
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-            addItemDecoration(
-                GridItemDecorationDiaryMain(
-                    resources.getDimensionPixelSize(R.dimen.component_margin_small),
-                    requireActivity()
-                )
-            )
-        }
+//        mBinding.recyclerDiary.run {
+//            adapter = mDiaryDashboardItemAdapter
+//            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+//            addItemDecoration(
+//                GridItemDecorationDiaryMain(
+//                    resources.getDimensionPixelSize(R.dimen.component_margin_small),
+//                    requireActivity()
+//                )
+//            )
+//        }
 
         mBinding.textTitle.text = when (arguments?.getString(MODE_FLAG, MODE_PREVIOUS_100)) {
             MODE_TASK -> "Task"
             else -> "Previous 100"
         }
-        refreshList()
+        setupDiary()
     }
 
-    private fun refreshList() {
+    override fun onResume() {
+        super.onResume()
+        Handler(Looper.getMainLooper()).post { updateDiary() }
+    }
+
+
+    /***************************************************************************************************
+     *   etc functions
+     *
+     ***************************************************************************************************/
+    private fun setupDiary() {
+        mBannerDiary = (mBinding.bannerViewPagerDiary as BannerViewPager<Diary>).apply {
+            setLifecycleRegistry(lifecycle)
+            adapter = DiaryDashboardItemAdapter()
+            setAutoPlay(false)
+            setInterval(3000)
+            setScrollDuration(800)
+            setPageMargin(requireContext().dpToPixel(5F))
+            setPageStyle(PageStyle.MULTI_PAGE_SCALE)
+            setRevealWidth(requireContext().dpToPixel(20F))
+            FigureIndicatorView(requireContext()).apply {
+                setRadius(resources.getDimensionPixelOffset(R.dimen.dp_18))
+                setTextSize(resources.getDimensionPixelOffset(R.dimen.sp_13))
+                setBackgroundColor(config.primaryColor)
+                setIndicatorGravity(IndicatorGravity.END)
+                setIndicatorView(this)
+            }
+        }
+    }
+
+    private fun updateDiary() {
         mDiaryList.clear()
         val diaryList: List<Diary> = when (arguments?.getString(MODE_FLAG, MODE_PREVIOUS_100)) {
             MODE_TASK -> EasyDiaryDbHelper.findDiary(null, config.diarySearchQueryCaseSensitive, 0, 0, 0).filter { item -> item.weather in 80..83 }
             else -> EasyDiaryDbHelper.findDiary(null, config.diarySearchQueryCaseSensitive, 0, 0, 0).subList(0, 100)
         }
         mDiaryList.addAll(diaryList)
-        mDiaryDashboardItemAdapter?.notifyDataSetChanged()
+        mBannerDiary.run {
+            create(mDiaryList)
+        }
     }
 
     companion object {
