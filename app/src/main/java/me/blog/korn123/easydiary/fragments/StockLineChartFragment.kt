33094454cf11b0cc0df096daf01695a32ba3b002
MountRigi <awesomeluzern@yandex.com>
2022-12-30 08:12:40 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easydiary/fragments/StockLineChartFragment.kt
+++ app/src/main/java/me/blog/korn123/easydiary/fragments/StockLineChartFragment.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.view.marginBottom
 import com.github.mikephil.charting.charts.CombinedChart
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.AxisBase
