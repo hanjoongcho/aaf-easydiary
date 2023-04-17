62fa824f4a4e80d1fa2528bcf9001de6ce911e78
Hanjoong.Cho <hanjoongcho@gmail.com>
2023-04-18 00:51:36 +0900

    Update release information

--- app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
@@ -58,6 +58,7 @@ open class EasyDiaryActivity : BaseSimpleActivity() {
 
     fun checkWhatsNewDialog(applyFilter: Boolean = true) {
         arrayListOf<Release>().apply {
+            add(Release(295, R.string.release_295))
             add(Release(294, R.string.release_294))
             add(Release(293, R.string.release_293))
             add(Release(292, R.string.release_292))
