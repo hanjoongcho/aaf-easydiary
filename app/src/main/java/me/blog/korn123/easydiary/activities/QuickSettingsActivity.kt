65eaab4072905a4f57493073437a95ba21f38e97
Hanjoong.Cho <hanjoongcho@gmail.com>
2024-05-25 08:56:59 +0900

    Customize compose TopBar

--- app/src/main/java/me/blog/korn123/easydiary/activities/QuickSettingsActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/QuickSettingsActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -27,6 +28,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
--- app/src/main/java/me/blog/korn123/easydiary/ui/components/TopAppBar.kt
+++ app/src/main/java/me/blog/korn123/easydiary/ui/components/TopAppBar.kt
@@ -1,16 +1,21 @@
 package me.blog.korn123.easydiary.ui.components
 
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import me.blog.korn123.commons.utils.FontUtils
@@ -20,13 +25,37 @@ import me.blog.korn123.easydiary.extensions.config
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EasyDiaryActionBar(textUnit: TextUnit, close: () -> Unit) {
+    val isDarkMode = isSystemInDarkTheme()
+    val context = LocalContext.current as ComponentActivity
+
+    DisposableEffect(isDarkMode) {
+        context.enableEdgeToEdge(
+            statusBarStyle = if (!isDarkMode) {
+//                SystemBarStyle.light(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT)
+                SystemBarStyle.auto(context.config.primaryColor, context.config.primaryColor)
+            } else {
+                SystemBarStyle.auto(context.config.primaryColor, context.config.primaryColor)
+            },
+//            navigationBarStyle = if(!isDarkMode){
+//                SystemBarStyle.light(
+//                    navigationBarLight,
+//                    navigationBarDark
+//                )
+//            } else {
+//                SystemBarStyle.dark(navigationBarDark)
+//            }
+        )
+
+        onDispose { }
+    }
+
     TopAppBar(
         title = {
             Text(
                 text = "내 액션바",
                 style = TextStyle(
                     fontFamily = FontUtils.getComposeFontFamily(LocalContext.current),
-                    color = Color(LocalContext.current.config.textColor),
+                    color = Color.White,
                     fontSize = TextUnit(textUnit.value, TextUnitType.Sp),
                 ),
             )
@@ -39,5 +68,12 @@ fun EasyDiaryActionBar(textUnit: TextUnit, close: () -> Unit) {
                 )
             }
         },
+        colors = TopAppBarColors(
+            Color(LocalContext.current.config.primaryColor),
+            Color(LocalContext.current.config.primaryColor),
+            Color.White,
+            Color.White,
+            Color.White,
+        )
     )
 }
