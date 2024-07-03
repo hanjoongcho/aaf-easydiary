58f95f3c3415be8e67ddc2b9a6cf53d460d7c89c
Hanjoong.Cho <hanjoongcho@gmail.com>
2024-07-03 20:46:50 +0900

    deploy: Update version code

--- app/src/main/java/me/blog/korn123/easydiary/activities/BaseDevActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/BaseDevActivity.kt
@@ -32,7 +32,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -45,7 +44,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
