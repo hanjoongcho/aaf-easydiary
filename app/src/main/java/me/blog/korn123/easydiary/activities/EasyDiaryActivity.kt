cb480dd11193563e406085cc2511e669477398aa
Hanjoong.Cho <hanjoongcho@gmail.com>
2024-10-24 21:39:12 +0900

    deploy: Update release information

--- app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
@@ -92,6 +92,7 @@ open class EasyDiaryActivity : BaseSimpleActivity(), ShakeDetector.Listener {
      ***************************************************************************************************/
     fun checkWhatsNewDialog(applyFilter: Boolean = true) {
         arrayListOf<Release>().apply {
+            add(Release(317, R.string.release_317))
             add(Release(313, R.string.release_313))
             add(Release(312, R.string.release_312))
             add(Release(309, R.string.release_309))
