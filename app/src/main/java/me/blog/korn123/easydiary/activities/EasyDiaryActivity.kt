306b78ae79bb6b4f1a9c0a32abd6cb128e627d90
Hanjoong.Cho <hanjoongcho@gmail.com>
2025-02-03 00:37:25 +0900

    deploy: Update version code

--- app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
+++ app/src/main/java/me/blog/korn123/easydiary/activities/EasyDiaryActivity.kt
@@ -92,6 +92,7 @@ open class EasyDiaryActivity : BaseSimpleActivity(), ShakeDetector.Listener {
      ***************************************************************************************************/
     fun checkWhatsNewDialog(applyFilter: Boolean = true) {
         arrayListOf<Release>().apply {
+            add(Release(326, R.string.release_326))
             add(Release(325, R.string.release_325))
             add(Release(324, R.string.release_324))
             add(Release(319, R.string.release_319))
