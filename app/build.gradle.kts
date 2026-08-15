ad6a1fbc22aaa89f1a3a2bdc399cae7e42c5db50
Hanjoong.Cho <hanjoongcho@gmail.com>
2026-08-15 18:35:13 +0900

    chore(release): update release information

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "me.blog.korn123.easydiary"
         minSdk = 26
         targetSdk = appCompileSdk
-        versionCode = 348
-        versionName = "1.4.348.202608120"
+        versionCode = 349
+        versionName = "1.4.349.202608150"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
